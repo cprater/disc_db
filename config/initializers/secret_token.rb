91079ce9b0e3117b13c02261d632be08cdc7dfc2
Chris Prater <cprater23@gmail.com>
2014-06-25 12:50:25 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiscDb::Application.config.secret_key_base = '2d4366ce01588c57c84e1c8d3f544602571185367f59b05e39d0e5f4a86ea8c016b89180de0aae01671c71f1abfe48108c2b8ce249180e7dcf9603b8467f0d78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiscDb::Application.config.session_store :cookie_store, key: '_disc_db_session'
